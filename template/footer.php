63874f08cc5bb45a85f3b82b59e4a81817925c2c
Changli Gao <xiaosuo@gmail.com>
2012-09-24 19:17:14 +0800

    Remove the unused code for buttons

--- template/footer.php
+++ template/footer.php
@@ -1,13 +1,2 @@
-<script>
-$(document).ready(function() {
-	$('button').each(function(i) {
-		$(this).click(function() {
-			var href = window.location.href;
-			href = href.substr(0, href.lastIndexOf("/") + 1) + $(this).attr("href");
-			window.location.assign(href);
-		});
-	});
-});
-</script>
 </body>
 </html>
