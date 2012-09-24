a0d41440606f4bd4111547c4a14973c62f9ffaa2
Changli Gao <xiaosuo@gmail.com>
2012-09-24 19:17:15 +0800

    Update the configurations list

--- template/list.php
+++ template/list.php
@@ -5,50 +5,66 @@
 <table>
 	<caption>Configurations</caption>
 	<tr>
-		<th class="action">
+		<th class="action" rowspan="2">
 			<a href="edit.php"><img src="image/add.png" alt="Add"/></a>
 			<a href="list.php?del=all"><img name="delete-all" src="image/delete.png" alt="Delete All"/></a>
 		</th>
-		<th>MAC Address</th>
-		<th>Username</th>
-		<th>Switch</th>
-		<th>Gateway</th>
+		<th rowspan="2">MAC Address</th>
+		<th rowspan="2">Username</th>
+		<th colspan="2">Switch</th>
+		<th colspan="3">Gateway</th>
+	</tr>
+	<tr>
+		<th>Host</th>
+		<th>Call Limit</th>
+		<th>Prefix</th>
+		<th>Host</th>
+		<th>Port</th>
 	</tr>
 <?php
 foreach ($model as $config) {
+	$maxrowspan =  max(1, count($config["switch"]), count($config["gateway"]));
+	for ($rowspan = $maxrowspan, $i = 0; $rowspan > 0; $rowspan--, $i++) {
 ?>
 	<tr>
-		<th class="action"><a href="list.php?del=<?php echo urlencode($config["mac"]); ?>"><img name="delete" src="image/delete.png" alt="Delete"/></a></th>
-		<td><a href="edit.php?mac=<?php echo urlencode($config["mac"]); ?>"><?php echo $config["mac"]; ?></a></td>
-		<td><?php echo htmlspecialchars($config["username"]); ?></td>
-		<td>
-			<ul>
 <?php
-	foreach ($config["switch"] as $switch) {
+		if ($i == 0) {
 ?>
-				<li>
-					<?php echo $switch["host"]; ?>(<?php echo $switch["call-limit"]; ?>)
-				</li>
+		<th class="action" rowspan="<?php echo $rowspan; ?>"><a href="list.php?del=<?php echo urlencode($config["mac"]); ?>"><img name="delete" src="image/delete.png" alt="Delete"/></a></th>
+		<td rowspan="<?php echo $rowspan; ?>"><a href="edit.php?mac=<?php echo urlencode($config["mac"]); ?>"><?php echo $config["mac"]; ?></a></td>
+		<td rowspan="<?php echo $rowspan; ?>"><?php echo htmlspecialchars($config["username"]); ?></td>
 <?php
-	}
+		}
 ?>
-			</ul>
-		</td>
-		<td>
-			<ul>
 <?php
-	foreach ($config["gateway"] as $gateway) {
+		if ($i == 0 || $i < count($config["switch"])) {
+			if (count($config["switch"]) == 0 ||
+			    $i == count($config["switch"]) - 1)
+				$span = $rowspan;
+			else
+				$span = 1;
 ?>
-				<li>
-					<?php echo $gateway["prefix"] ?>-<?php echo $gateway["host"]; ?>:<?php echo $gateway["port"]; ?>
-				</li>
+		<td rowspan="<?php echo $span; ?>"><?php echo $config["switch"][$i]["host"]; ?></td>
+		<td rowspan="<?php echo $span; ?>"><?php echo $config["switch"][$i]["call-limit"]; ?></td>
 <?php
-	}
+		}
+
+		if ($i == 0 || $i < count($config["gateway"])) {
+			if (count($config["gateway"]) == 0 ||
+			    $i == count($config["gateway"]) - 1)
+				$span = $rowspan;
+			else
+				$span = 1;
 ?>
-			</ul>
-		</td>
+		<td rowspan="<?php echo $span; ?>"><?php echo htmlspecialchars($config["gateway"][$i]["prefix"]); ?></td>
+		<td rowspan="<?php echo $span; ?>"><?php echo $config["gateway"][$i]["host"]; ?></td>
+		<td rowspan="<?php echo $span; ?>"><?php echo $config["gateway"][$i]["port"]; ?></td>
+<?php
+		}
+?>
 	</tr>
 <?php
+	}
 }
 ?>
 </table>
