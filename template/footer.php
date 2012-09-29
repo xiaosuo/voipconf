4df1a82ea858309c248734085dbcbd8c812775a5
Changli Gao <xiaosuo@gmail.com>
2012-09-29 18:48:22 +0800

    Don't wrap text in table on IE6/IE7

--- template/footer.php
+++ template/footer.php
@@ -1,2 +1,11 @@
+<script>
+// work around for IE6 and IE7
+$(document).ready(function() {
+	$("th,td").each(function(i) {
+		if ($(this).children().length == 0)
+			$(this).html('<span class="nowrap">' + $(this).html() + '</span>');
+	});
+});
+</script>
 </body>
 </html>
